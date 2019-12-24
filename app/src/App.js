5bef0ea78cba9e50b5e8d40216ffcac804e658b5
Darkø Tasevski <darko.tasevski@redbox.com>
2019-12-24 11:06:40 +0100

    refactor: connect App with Redux Provider

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Provider } from 'react-redux';
+
+import store from './Redux';
 
 import "./App.css";
 
+const Root = () => (
+    <Provider store={store}>
+      <div className="App">Udri Joco!</div>
+    </Provider>
+);
+
 function App() {
-  return <div className="App">Udri Joco!</div>;
+  return <Root/>
 }
 
 export default App;
