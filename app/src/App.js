540fcaff2812a364a5f5a5dbc89f4195c4a170a1
Jovan M <tcdweb@gmail.com>
2019-12-24 14:55:23 +0100

    Fix: favicon and bg image + css tweaks

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { PageHeader } from 'antd';
+import { PageHeader, Row, Col } from 'antd';
 
 import RepoDetails from './components/RepoDetails/RepoDetails';
 import LoginButton from './components/Auth/LoginButton';
 import Auth from './components/Auth/Auth';
 import Search from './components/Search/Search';
 
-import './App.css';
 import 'antd/dist/antd.css';
+import './App.css';
 
 function App() {
 	return (
-		<main className="App">
-			<PageHeader className="Page__header" title="ContriHub">
-				<LoginButton />
-			</PageHeader>
+		<Row type="flex" justify="center">
+			<Col offset={1} span={22} className="App">
+				<PageHeader className="Page__header" title="ContribHub">
+					<LoginButton  />
+				</PageHeader>
 
-			<Route path="/auth" component={Auth} />
-			<Route path="/details" component={RepoDetails} />
-			<Route path="/landing" component={Search} />
-		</main>
+				<Route path="/auth" component={Auth} />
+				<Route path="/details" component={RepoDetails} />
+				<Route path="/" component={Search} />
+			</Col>
+		</Row>
 	);
 }
 
