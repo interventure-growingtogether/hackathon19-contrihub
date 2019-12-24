a0471ed3d74055c936d81b170abba13b08f80859
Danijel Maksimovic <danijel.maksimovic@redbox.com>
2019-12-24 13:26:21 +0100

    feature: designs slider

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import * as serviceWorker from './serviceWorker';
 import Root from './Root';
 
