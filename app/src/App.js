360256ff33895fd56b9ac8d112c0c8e204c7c28a
Darkø Tasevski <darko.tasevski@redbox.com>
2019-12-24 15:20:00 +0100

    refactor: handle loading repos on search page

--- app/src/App.js
+++ app/src/App.js
@@ -7,12 +7,13 @@ import RepoDetails from './components/RepoDetails/RepoDetails';
 import LoginButton from './components/Auth/LoginButton';
 import Auth from './components/Auth/Auth';
 import Search from './components/Search/Search';
+import Spinner from './components/Spinner/Spinner';
 
 import 'antd/dist/antd.css';
 import './App.css';
 import { getRepoData } from './Redux/actions/getRepoData';
 
-function App({ getRepoData, repos }) {
+function App({ getRepoData, repos, spinnerVisible }) {
 	useEffect(() => {
 		getRepoData();
 	}, []);
@@ -22,22 +23,27 @@ function App({ getRepoData, repos }) {
 	}, [repos]);
 
 	return (
-		<Row type="flex" justify="center">
-			<Col offset={1} span={22} className="App">
-				<PageHeader className="Page__header" title="ContribHub">
-					<LoginButton  />
-				</PageHeader>
+		<main>
+			<Row type="flex" justify="center">
+				<Col offset={1} span={22} className="App">
+					<PageHeader className="Page__header" title="ContribHub">
+						<LoginButton />
+					</PageHeader>
 
-				<Route path="/auth" component={Auth} />
-				<Route path="/details" component={RepoDetails} />
-				<Route path="/" component={Search} />
-			</Col>
-		</Row>
+					<Route path="/auth" component={Auth} />
+					<Route path="/details" component={RepoDetails} />
+					<Route path="/" component={Search} />
+				</Col>
+			</Row>
+
+			{spinnerVisible && <Spinner />}
+		</main>
 	);
 }
 
 const mapStateToProps = state => ({
 	repos: state.repos,
+	spinnerVisible: state.spinner.visible,
 });
 
 export default connect(mapStateToProps, { getRepoData })(App);
